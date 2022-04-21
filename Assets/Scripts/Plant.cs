9e0d0ac84980241c0f1be18ec7db170b736d9e55
Emiel131517 <84375895+Emiel131517@users.noreply.github.com>
2022-04-21 12:06:27 +0200

    added tulp scaling

---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    public float waterNeed;
+    //private Renderer renderer;
+    void Start()
+    {
+        //renderer = gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = 0;
+        time += Time.deltaTime;
+        if (time >= 1)
+        {
+            waterNeed -= 1;
+            time = 0;
+        }
+    }
+}
